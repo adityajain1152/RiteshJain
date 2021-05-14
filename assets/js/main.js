c62189f8c2c01612d80dd6baab9ce424f849d1b4
adityajain1152 <adityajain1152@gmail.com>
2021-05-14 15:28:44 +0530

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,35 +152,35 @@
   });
 
   // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
-    });
+  // $(window).on('load', function() {
+  //   var portfolioIsotope = $('.portfolio-container').isotope({
+  //     itemSelector: '.portfolio-item'
+  //   });
 
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
+  //   $('#portfolio-flters li').on('click', function() {
+  //     $("#portfolio-flters li").removeClass('filter-active');
+  //     $(this).addClass('filter-active');
 
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
+  //     portfolioIsotope.isotope({
+  //       filter: $(this).data('filter')
+  //     });
+  //   });
 
     // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox({
-        'share': false
-      });
-    });
-  });
+  //   $(document).ready(function() {
+  //     $('.venobox').venobox({
+  //       'share': false
+  //     });
+  //   });
+  // });
 
   // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+//   $(".portfolio-details-carousel").owlCarousel({
+//     autoplay: true,
+//     dots: true,
+//     loop: true,
+//     items: 1
+//   });
 
 })(jQuery);
 
