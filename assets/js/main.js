6568deff371626a99bb970afc954817197a4e7c1
adityajain1152 <adityajain1152@gmail.com>
2021-11-25 09:07:10 +0530

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,10 +146,10 @@
   });
 
   // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
+  // $('[data-toggle="counter-up"]').counterUp({
+  //   delay: 10,
+  //   time: 1000
+  // });
 
   // Porfolio isotope and filter
   // $(window).on('load', function() {
